3249f96f2606f1d96bac8d90444d134936439275
Ruben Burgos <code.rburgos@gmail.com>
2022-10-12 19:46:45 -0500

    dotnet exercise

---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Conan, what is best in life?");
+Console.WriteLine("To crush your enemies, to see them driven before you, and to hear the lamentations of their women.");
